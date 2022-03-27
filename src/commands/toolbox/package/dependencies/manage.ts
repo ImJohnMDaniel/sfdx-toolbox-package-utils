cd6904fa10e471c91c890388fe73eddf0003bea9
John M. Daniel <imjohnmdaniel@ce-v.com>
2022-03-27 14:38:15 -0400

    Changes related to recognizing other branches as releasable
    Changes made:
    * corrected redundant call to `this.project.retrieveSfdxProjectJson();` in order to find the dependencies to ignore.  Now, it simply calls new function `theSfdxProject.getProjectDependenciesToIgnore();`
    * calls new `theSfdxProject.getBranchNamesThatContainReleasedVersions();` method and forwards the contents to two different `DevHubDependencies` methods

--- src/commands/toolbox/package/dependencies/manage.ts
+++ src/commands/toolbox/package/dependencies/manage.ts
@@ -55,8 +55,9 @@ export default class Manage extends SfdxCommand {
 
     const packageDependencyChangeMap: Map<string, ProjectDependencyChange[]> = new Map<string, ProjectDependencyChange[]>();
 
-    const projectJson = await this.project.retrieveSfdxProjectJson();
-    let dependenciesToIgnore = _.get(projectJson['contents'], 'plugins.toolbox.dependencies.ignore', false) as string[];
+    const dependenciesToIgnore = theSfdxProject.getProjectDependenciesToIgnore();
+
+    const branchNamesThatContainReleasedVersions = theSfdxProject.getBranchNamesThatContainReleasedVersions();
 
     const evaluateOptions = async () => {
       let aProjectDependencyChange: ProjectDependencyChange;
@@ -104,11 +105,11 @@ export default class Manage extends SfdxCommand {
               dependencyPackageChoices = theDevHubDependencies.prepareSameDependencyOptionForCurrentDependency();
             } else if (isInteractiveMode) {
               //
-              dependencyPackageChoices = theDevHubDependencies.prepareRelatedDependencyOptionsForCurrentDependency();
+              dependencyPackageChoices = theDevHubDependencies.prepareRelatedDependencyOptionsForCurrentDependency(branchNamesThatContainReleasedVersions);
             } else {
               if ( this.flags.updatetoreleased ) {
                 //
-                dependencyPackageChoices = theDevHubDependencies.prepareRelatedReleasedDependencyOptionsForCurrentDependency();
+                dependencyPackageChoices = theDevHubDependencies.prepareRelatedReleasedDependencyOptionsForCurrentDependency(branchNamesThatContainReleasedVersions);
               } else if (this.flags.updatetolatest) {
                 dependencyPackageChoices = theDevHubDependencies.prepareRelatedNonPinnedDependencyOptionsForCurrentDependency();
               }
