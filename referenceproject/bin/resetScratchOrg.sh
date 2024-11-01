27715f19c0d4078705b1034ca878b8cd375b9713
John M. Daniel <imjohnmdaniel@ce-v.com>
2024-11-01 12:49:40 -0400

    updates and clean up to project to make ready to start work on SFv2 upgrade.

---
+++ referenceproject/bin/resetScratchOrg.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sf org delete scratch --no-prompt --target-org toolboxpu1
+sf org create scratch --alias toolboxpu1 --definition-file config/project-scratch-def.json --set-default --wait 20 --duration-days 1
+# ../bin/run.js toolbox package dependencies install --wait 120
--- src/types/inquirer_option.ts
+++
@@ -1,5 +0,0 @@
-export class InquirerOption {
-    public name: string;
-    public value: string;
-    public short: string;
-}
