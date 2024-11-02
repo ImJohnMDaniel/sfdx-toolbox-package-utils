49e17e900d009d5a3ac9b4dd2b9426fb0e9e77de
John M. Daniel <imjohnmdaniel@ce-v.com>
2024-11-02 15:53:07 -0400

    minor tweak

--- referenceproject/bin/resetScratchOrg.sh
+++ referenceproject/bin/resetScratchOrg.sh
@@ -2,4 +2,4 @@
 
 sf org delete scratch --no-prompt --target-org toolboxpu1
 sf org create scratch --alias toolboxpu1 --definition-file config/project-scratch-def.json --set-default --wait 20 --duration-days 1
-# ../bin/run.js toolbox package dependencies install --wait 120
+../bin/run.js toolbox package dependencies install --wait 120
